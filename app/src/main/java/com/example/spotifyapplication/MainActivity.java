99532e29a7ab13d9a289f9ea4e8c08c9be9ec49b
Mark <01markhuynh@gmail.com>
2020-02-27 20:55:48 -0800

    Basic implementation of time_range

--- app/src/main/java/com/example/spotifyapplication/MainActivity.java
+++ app/src/main/java/com/example/spotifyapplication/MainActivity.java
@@ -14,7 +14,10 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.spotifyapplication.utils.NetworkUtils;
 import com.example.spotifyapplication.utils.SpotifyUtils;
@@ -26,17 +29,30 @@ import java.util.Arrays;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private RecyclerView mTracksRV;
     private TrackAdapter mTrackAdapter;
     private Button loginButton;
     private SpotifyViewModel mViewModel;
 
+    private Spinner timeRangeSpinner;
+
+    private String timeRange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeRangeSpinner = findViewById(R.id.time_range_spinner);
+
+        ArrayList<String> timeRangeOptions = new ArrayList<>(Arrays.asList("Several Years", "Last 6 Months", "Last 4 Weeks"));
+        ArrayAdapter<String> timeRangeSpinnerArr = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, timeRangeOptions);
+
+        timeRangeSpinnerArr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeRangeSpinner.setAdapter(timeRangeSpinnerArr);
+        timeRangeSpinner.setOnItemSelectedListener(this);
 
         mTracksRV = findViewById(R.id.rv_tracks_list);
 
@@ -65,8 +81,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doTopTracksQuery(){
-        mViewModel.loadSearchResults("50", "short_term");
+        mViewModel.loadSearchResults("50", timeRange);
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String range = parent.getItemAtPosition(position).toString();
+        Log.d("MAIN", range);
+        if(range.equals("Several Years")) {
+            timeRange = "long_term";
+        } else if(range.equals("Last 6 Months")) {
+            timeRange = "medium_term";
+        } else if(range.equals("Last 4 Weeks")) {
+            timeRange = "short_term";
+        }
+        doTopTracksQuery();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
